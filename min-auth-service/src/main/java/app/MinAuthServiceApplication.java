88edcb1d113d2abb3a3182bb6ad39c385f0cdeeb
유윤열(Yoonyoul, Yoo)/융합서비스개발그룹/SK <ryukato@sk.com>
2017-11-09 22:04:46 +0900

    Modules - user-command, user-resource, auth and user-domain

---
+++ min-auth-service/src/main/java/app/MinAuthServiceApplication.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinAuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinAuthServiceApplication.class, args);
+    }
+}
---
+++ min-user-resource-service/src/main/java/app/MinUserResourceServiceApplication.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinUserResourceServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinUserResourceServiceApplication.class, args);
+    }
+}
